5b56a561d1542d910cc7cf86f9b52f9823010653
Sergey Shevtsov <sergeyprostoas@gmail.com>
2018-11-13 17:46:08 +0300

    Редакция медиа запросов

--- app/js/common.js
+++ app/js/common.js
@@ -6,21 +6,14 @@ $(".nav--search-hide").click(function(){
 	$(".nav--search-block").hide();
 });
 
-$(document).ready(function() {
-	var owlHeader = $('.slider');
-	owlHeader.owlCarousel({
-		loop: true,
-		autoplay: true,
-		slideSpeed: 300,
-		paginationSpeed: 400,
-		autoplayHoverPause: true,
-		animateIn: 'fadeIn',
-		animateOut: 'fadeOut',
-		nav: true,
-		navText : ["<img src='img/slider_arrow_l.png'>","<img src='img/slider_arrow_r.png'>"],
-		dots: true,
-		items: 1
-	});
+$('.slider').owlCarousel({
+    loop:true,
+    margin:30,
+    autoplay: true,
+	slideSpeed: 300,
+	paginationSpeed: 400,
+	autoplayHoverPause: true,
+    items: 1
 })
 
 $('.block-slider').owlCarousel({
@@ -31,7 +24,7 @@ $('.block-slider').owlCarousel({
 	paginationSpeed: 400,
     responsive:{
         0:{
-            items:1
+            items:2
         },
         1200:{
             items:5
