e5c299bcdcedba3f377f1701ed081f45b4b24d80
Sergeyprostoas <Sergeyprostoas@gmail.com>
2018-11-27 08:29:38 +0300

    Редактирование под WP

--- app/js/common.js
+++ app/js/common.js
@@ -148,3 +148,24 @@
       
       lastScrollTop = st;
   }
+
+// Ayax search
+  jQuery(document).ready(function($){
+    $('.nav--search').keypress(function(eventObject){
+      var searchTerm = $(this).val();
+      // проверим, если в поле ввода более 2 символов, запускаем ajax
+      if(searchTerm.length > 2){
+        $.ajax({
+          url : '/wp-admin/admin-ajax.php',
+          type: 'POST',
+          data:{
+            'action':'codyshop_ajax_search',
+            'term'  :searchTerm
+          },
+          success:function(result){
+            $('.codyshop-ajax-search').fadeIn().html(result);
+          }
+        });
+      }
+    });
+  });
